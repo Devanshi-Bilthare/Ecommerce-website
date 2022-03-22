be161a308bac727b89f5c8c690ed49f693310d2f
Devanshi Bilthare <devanshibilthare@gmail.com>
2022-03-22 11:04:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const bar = document.getElementById('bar');
+const navbar = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+bar.addEventListener('click',function(){
+    navbar.style.right = 0;
+})
+
+close.addEventListener('click',function(){
+    navbar.style.right = '-300px';
+})
+
+// single product
+
+var MainImg = document.getElementById('MainImg');
+var smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function(){
+    MainImg.src = smallImg[0].src
+}
+smallImg[1].onclick = function(){
+    MainImg.src = smallImg[1].src
+}
+smallImg[2].onclick = function(){
+    MainImg.src = smallImg[2].src
+}
+smallImg[3].onclick = function(){
+    MainImg.src = smallImg[3].src
+}
